40cdf47a23dafcdc27406fb7f8e88bf4adb46cdb
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-09 13:49:12 -0700

    commit first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if( n > 0 ) {
 	   printf("is positive\n")
-	} else if( n = 0 ) {
+	}
+       	else if( n = 0 ) {
 	   printf("is zero\n")
-	} else {
+	}
+       	else {
 	   printf("is negative\n")
 	}
 	return (0);
