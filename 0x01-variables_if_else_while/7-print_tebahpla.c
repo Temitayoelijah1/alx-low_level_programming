9c217c015480601dcded04e177eb1d2239032965
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-12 16:37:54 -0700

    comb

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - program that prints the lowercase in the reverse.
- * You can only use putchar function
- * return 0
- */
-
-int main(void)
-{
-char low;
-for (low = 'z'; low >= 'a'; low =--)
-putchar(low);
-putchar('\n');
-return (0);
-}
