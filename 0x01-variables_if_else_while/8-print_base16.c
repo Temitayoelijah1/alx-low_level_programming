b3c3a5424ffa4242886544a76cff898c1f437793
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-12 18:10:24 -0700

    base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,20 +6,17 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
 int d;
+char low;
 for (d = '0'; d <= '9'; d++)
-{
 putchar(d);
-if (d != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
 putchar('\n');
 return (0);
 }
