c494dc00206432b7fe45c7777e8fd6c009b4a2ed
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 15:35:03 -0700

    coming

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,22 +6,23 @@
  */
 void print_triangle(int size)
 {
+	int x, y, z;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
+		for (x = 0; x <= size; x++)
 		{
-			for (j = i; j < size; j++)
-			{																				_putchar(' ');
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
 			}
 
-			for (j = 1; j <= i; j++)
+			for (z = 0; z <= i; z++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 
 			_putchar('\n');
