5e27131abad4ba0cae8f75553ea2c3711cf1cde2
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 05:57:50 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,12 @@
  * print_most_numbers - print the numbers from 0 to 9,
  * followed by a new line.
  * Decription: Prints the numbers excluding 2 and 4
- *
- * Return: the numbers from 0 to 9
  */
 void print_most_numbers(void)
 {
 int x = 0;
 
-for (; x <= 9; x++)
+for ( x <= 9; x++)
 {
 if (x == 2 || x == 4)
 {
