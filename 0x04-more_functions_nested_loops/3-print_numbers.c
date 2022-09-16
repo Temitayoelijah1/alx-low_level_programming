1c2a862a27c3f40704b556b9f49334f21c8e0d4a
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 05:07:46 -0700

    print num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- *
+ * print_numbers - Print the numbers, from 0 to 9,
+ * followed by a newline
  * Return: The numbers since 0 up to 9
  */
 void print_numbers(void)
 {
 int x;
 
-for (x = 0; x <= 9; x++)
+for (x = 0; x < 10; x++)
 {
 _putchar(x + '0');
 }
