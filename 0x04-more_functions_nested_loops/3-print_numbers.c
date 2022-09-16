f89a3e6161cdd07f6b13dd1f4ab936047b3c992d
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 05:16:30 -0700

    num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
  * print_numbers - Print the numbers, from 0 to 9,
  * followed by a newline
- * Return: The numbers since 0 up to 9
  */
+
 void print_numbers(void)
 {
-int x;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
 
-for (x = 0; x < 10; x++)
-{
-_putchar(x + '0');
-}
-
-_putchar('\n');
+	_putchar('\n');
 }
