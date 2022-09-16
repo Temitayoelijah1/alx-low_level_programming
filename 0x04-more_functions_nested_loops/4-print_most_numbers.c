fbb22770ba44385fc920155d3167c5d38c7d0c45
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 11:09:29 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@
 
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for ( x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		if (x == 2 || x == 4)
 		{
@@ -18,7 +18,7 @@ void print_most_numbers(void)
 		}
 		else
 		{
-			_putchar(x + '0');
+			_putchar('0' + x);
 		}
 	}
 	_putchar('\n');
