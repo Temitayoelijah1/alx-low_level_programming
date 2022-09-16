08300b585c6cf61335c8aa85b0812bec38a8b751
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-16 06:41:53 -0700

    git

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers, from 0 to 9,
- * followed by a newline.
+ * print_numbers - Print the numbers from 0 to 9,
+ * followed by a newline
  */
 
 void print_numbers(void)
