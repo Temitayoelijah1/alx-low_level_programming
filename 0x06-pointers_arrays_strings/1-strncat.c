4018ee4afa7314a294283dd73f2475bb15bf37b7
Olabode Temitayo Elijah <lilstylz4real@gmail.com>
2022-09-22 17:52:36 -0700

    cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,9 @@
  *
  * Return: pointer to the resulting string
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	dest = strncat(dest, src, n);
 
-	i = 0;
-
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
 	return (dest);
 }
